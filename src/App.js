c98b711e83b1a7b1d280036676e040ddf7cfafdb
Safet Salkanović <salkanovicsafet@gmail.com>
2023-05-16 23:31:25 +0200

    Change up the colors and make it look better in general

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import './assets/styles/App.css';
+import React, { useState } from "react";
+import "./assets/styles/App.css";
 
 export default function App() {
   const [inputs, setInputs] = useState({
-    text1: '',
-    text2: '',
+    text1: "",
+    text2: "",
   });
   const [convertedInputs, setConvertedInputs] = useState({
-    text1: '',
-    text2: '',
+    text1: "",
+    text2: "",
   });
   const [checkingMode, setCheckingMode] = useState(false);
 
@@ -22,7 +22,7 @@ export default function App() {
   function check() {
     setCheckingMode(!checkingMode);
     setConvertedInputs({ ...convertedInputs, text1: inputs.text1 });
-    let editedText = '';
+    let editedText = "";
     convertedInputs.text1 = inputs.text1;
     if (inputs.text1.length >= inputs.text2.length) {
       for (let index = 0; index < inputs.text1.length; index++) {
@@ -54,8 +54,8 @@ export default function App() {
       }
     }
     if (
-      inputs.text1.toLowerCase() == 'haneen' &&
-      inputs.text2.toLowerCase() == 'slay queen'
+      inputs.text1.toLowerCase() == "haneen" &&
+      inputs.text2.toLowerCase() == "slay queen"
     )
       editedText = '<span class="green">slay queen</span>';
     setConvertedInputs({ ...convertedInputs, text2: editedText });
@@ -70,23 +70,23 @@ export default function App() {
           value={inputs.text1}
           onChange={handleChange}
           spellCheck="false"
-          className={`${checkingMode && 'hidden'} textarea`}
+          className={`${checkingMode && "hidden"} textarea`}
         />
-        <div className={`${!checkingMode && 'hidden'} checked-text`}>
+        <div className={`${!checkingMode && "hidden"} checked-text`}>
           <pre>{convertedInputs.text1}</pre>
         </div>
         <button className="btn" onClick={check}>
-          {checkingMode ? 'EDIT' : 'CHECK'}
+          {checkingMode ? "EDIT" : "CHECK"}
         </button>
         <textarea
           name="text2"
           value={inputs.text2}
           onChange={handleChange}
           spellCheck="false"
-          className={`${checkingMode && 'hidden'} textarea`}
+          className={`${checkingMode && "hidden"} textarea`}
         />
         <div
-          className={`${!checkingMode && 'hidden'} checked-text`}
+          className={`${!checkingMode && "hidden"} checked-text`}
           dangerouslySetInnerHTML={{
             __html: `<pre>${convertedInputs.text2}</pre>`,
           }}
